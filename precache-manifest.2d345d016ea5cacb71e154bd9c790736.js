d83f00e3a64a25e4a94f63b4ac7e308836c5b41b
Kirill Osypov <Kirill.Osypov@gmail.com>
2021-07-14 16:40:46 +0300

    Updates

---
+++ precache-manifest.2d345d016ea5cacb71e154bd9c790736.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15dee154877fc8deebfb920ce313aa00",
+    "url": "/react_people-table-advanced/index.html"
+  },
+  {
+    "revision": "f1c850d99e3e742b5b0a",
+    "url": "/react_people-table-advanced/static/css/main.4c8c17b8.chunk.css"
+  },
+  {
+    "revision": "1b1c45d91405eef1145d",
+    "url": "/react_people-table-advanced/static/js/2.1d89871a.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/react_people-table-advanced/static/js/2.1d89871a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1c850d99e3e742b5b0a",
+    "url": "/react_people-table-advanced/static/js/main.744d24f6.chunk.js"
+  },
+  {
+    "revision": "c9836782c33f907f6080",
+    "url": "/react_people-table-advanced/static/js/runtime-main.072d365a.js"
+  },
+  {
+    "revision": "a972033395c631d8be176654a01f2dff",
+    "url": "/react_people-table-advanced/static/media/svg-2.a9720333.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f217520e9a7c5c43549416669efde7cd.js"
+  "/react_people-table-advanced/precache-manifest.2d345d016ea5cacb71e154bd9c790736.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_people-table-advanced/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
